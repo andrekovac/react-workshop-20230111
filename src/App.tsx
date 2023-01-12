efab4b7d6269712b48925d50498472ce3837e136
André Kovac <me@andrekovac.com>
2023-01-12 13:12:52 +0100

    Task 3: BookListItem + demonstrate map and array rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+
 import { MyOldFashionedComponent } from "./components/OldFashionedComponent";
 import { AppHeader, myString } from "./components/AppHeader";
-import { ExampleBook } from "./components/ExampleBook";
+import { BookListItem } from "./components/BookListItem";
+
+import { exampleBooks } from "./domain/book/exampleBooks";
+import { Book } from "./domain/book";
+
+const bookMapping = (book: Book) => {
+  return <BookListItem key={book.isbn} book={book} />;
+};
 
 const App = () => {
   return (
@@ -9,7 +17,13 @@ const App = () => {
       <AppHeader title="My Book Shop" />
       <MyOldFashionedComponent />
       <p>{myString}</p>
-      <ExampleBook />
+      {exampleBooks.map(bookMapping)}
+      {/* {[
+        <BookListItem book={exampleBooks[0]} />,
+        <BookListItem book={exampleBooks[1]} />,
+        <BookListItem book={exampleBooks[2]} />,
+        <BookListItem book={exampleBooks[3]} />,
+      ]} */}
     </div>
   );
 };
