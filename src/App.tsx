6ae0d357816a237235b4cea280deb23755da2ace
André Kovac <me@andrekovac.com>
2023-01-13 11:22:48 +0100

    Counter example

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,36 @@ import { BookListItem } from "./components/BookListItem";
 import { exampleBooks } from "./domain/book/exampleBooks";
 import { Book } from "./domain/book";
 import { BookList } from "./components/BookList";
+import { useState } from "react";
 
 // const bookMapping = (book: Book) => {
 //   return <BookListItem key={book.isbn} book={book} />;
 // };
 
+const Counter = () => {
+  // let count = 0;
+  const [count, setCount] = useState(0);
+
+  console.log("render", { count });
+
+  const handleClick = () => {
+    setCount(count + 1);
+
+    console.log("Clicked", { count });
+  };
+
+  return (
+    <>
+      <button onClick={handleClick}>Increment</button>
+      <div>{count}</div>
+    </>
+  );
+};
+
 const App = () => {
   return (
     <div className="App">
+      <Counter />
       <AppHeader title="My Book Shop" />
       <MyOldFashionedComponent />
       <p>{myString}</p>
