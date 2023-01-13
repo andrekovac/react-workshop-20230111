fb38baff22e3b6bb49856c7683711b2ad369c6fb
André Kovac <me@andrekovac.com>
2023-01-13 11:22:41 +0100

    Task 5: Conditional rendering

--- src/components/BookListItem/BookListItem.tsx
+++ src/components/BookListItem/BookListItem.tsx
@@ -1,14 +1,21 @@
 import { Book } from "../../domain/book";
+import "./BookListItem.css";
 
 export interface BookListItemProps {
   book: Book;
 }
 export const BookListItem = ({ book }: BookListItemProps) => {
+  const isFree = book.price === "$0.00";
+
   return (
-    <>
-      <h2>{book.title}</h2>
+    <div>
+      {/* <h2 style={{ textDecoration: isFree ? "underline" : "none" }}> */}
+      <h2 className={isFree ? "underline" : undefined}>
+        {book.title}
+        {isFree && <span> &#x1F4B0;</span>}
+      </h2>
       <h3>{book.subtitle}</h3>
       <div>by {book.author}</div>
-    </>
+    </div>
   );
 };
