d42e260e9d9fb58408c20b59203157290526fd73
André Kovac <me@andrekovac.com>
2023-01-17 11:00:17 +0100

    ref demo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { MyOldFashionedComponent } from "./components/OldFashionedComponent";
 import { AppHeader, myString } from "./components/AppHeader";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Theme, ThemeContext } from "./domain/theme";
 import { ThemeEditor } from "./components/ThemeEditor";
 import { Outlet } from "react-router-dom";
@@ -19,8 +19,34 @@ const useCount = (): [boolean, () => void] => {
   return [showCounter, increment];
 };
 
+const UncontrolledForm = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(inputRef.current!.value);
+      }}
+    >
+      <input type="text" placeholder="Title" ref={inputRef} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
 const App = () => {
   const [showCounter, increment] = useCount();
+
+  const meinDiv = useRef<HTMLDivElement>(null);
+  // const meineZahl = useRef<number>(0);
+  // const [myNumber, setMyNumber] = useState(0);
+
+  useEffect(() => {
+    console.log("referenz des div elements", meinDiv.current?.innerHTML);
+
+    // meineZahl.current++;
+    // setMyNumber(myNumber + 1);
+  }, []);
 
   const [primaryColor, setPrimaryColor] = useState("tomato");
   const theme: Theme = {
@@ -35,6 +61,18 @@ const App = () => {
         <div>
           <button onClick={increment}>Show/Hide counter</button>
         </div>
+
+        <div ref={meinDiv}>Das ist ein neues Element</div>
+
+        <div
+          style={{
+            border: "3px solid black",
+            padding: "10px",
+          }}
+        >
+          <UncontrolledForm />
+        </div>
+
         {showCounter && <Counter />}
         <AppHeader title="My Book Shop" />
         <MyOldFashionedComponent />
